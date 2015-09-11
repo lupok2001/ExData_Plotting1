d233860deecc205c4d31e78cc766a765a67d2519
lupok2001 <lupo.kom@tiscalinet.it>
2015-09-11 19:32:16 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -12,10 +12,19 @@ data$Time <- strptime(data$Time, "%d/%m/%Y %H:%M:%S")
 data <- data[, c(2:ncol(data))]
 data <- subset(data, data$Time >= "2007-02-01 00:00:00" & data$Time < "2007-02-03 00:00:00")
 
-#Plot
+#Plot 1
+par(mfcol = c(2,2))
+plot(data$Time, data$Global_active_power, type = "l", xlab ="", ylab = "Global Active Power") 
+#Plot2
 plot(data$Time, data$Sub_metering_1, type = "l", xlab ="", ylab = "Energy sub metering") 
 points(data$Time, data$Sub_metering_2, col = "red", type = "l")
 points(data$Time, data$Sub_metering_3, col = "blue", type = "l")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1, col = c("Black","Red","Blue"))
-dev.copy(png, file = "plot3.png")
+#Plot3
+plot(data$Time, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage") 
+#Plot4
+plot(data$Time, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power") 
+
+dev.copy(png, file = "plot4.png")
 dev.off() 
+
